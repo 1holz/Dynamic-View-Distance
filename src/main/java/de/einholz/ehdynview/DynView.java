b7e22c4f9f3f60792fd514fb1a6e920540554089
Albert-Einholz <64534803+Albert-Einholz@users.noreply.github.com>
2022-08-11 17:25:02 -0400

    add No Chat Report Warning

--- src/main/java/de/einholz/ehdynview/DynView.java
+++ src/main/java/de/einholz/ehdynview/DynView.java
@@ -10,7 +10,17 @@ import net.fabricmc.loader.api.FabricLoader;
 
 public final class DynView implements ModInitializer, ClientModInitializer, DedicatedServerModInitializer {
     @Override
-    public void onInitialize() {}
+    public void onInitialize() {
+        if (FabricLoader.getInstance().isModLoaded("nochatreports")) {
+            System.out.println("===================================");
+            System.out.println("You don't have No Chat Reports installed!");
+            System.out.println("This way players can unjustifiedly get banned from Minecraft.");
+            System.out.println("For more information see https://saveminecraft.org/");
+            System.out.println("To prevent this from happening I recommend to install No Chat Reports. It is availabel at:");
+            System.out.println("https://www.curseforge.com/minecraft/mc-mods/no-chat-reports");
+            System.out.println("===================================");
+        }
+    }
 
     @Environment(EnvType.CLIENT)
     @Override
